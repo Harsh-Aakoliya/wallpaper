aa10c37d00cfd4b8f8bc081174d43a9d759110b0
Harsh <harsh@Harshs-MacBook-Air.local>
2026-07-15 23:26:53 +0530

    Added stack navigation

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -7,7 +7,24 @@ export default function RootLayout() {
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
         <SafeAreaView style={{ flex: 1 }}>
-          <Slot />
+          <Stack >
+            <Stack.Screen
+              name="(tabs)"
+              options={{
+                headerShown:false //this will make sure that (tabs) will not be shown as header for all bottom tabs
+              }}
+              />
+              {/* <Stack.Screen
+              name="(tabs)/account"
+              options={{
+                headerShown:true
+              }}
+              />
+                this is not the way to show header for account bottom bar because
+                the header shown properties account bottom bar is owned by _layout of (tabs) folder 
+                not by this _layout folder
+              */}
+            </Stack>
         </SafeAreaView>
       </SafeAreaProvider>
     </GestureHandlerRootView>
